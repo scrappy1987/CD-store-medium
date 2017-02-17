80f5cf0336c2d04fc27949da96aa2f3ea232de53
imannion12 <manyon05@outlook.com>
2017-02-17 15:56:00 +0000

    CdStore

--- src/main/java/com/qa/cdstore/service/CDDBImpl.java
+++ src/main/java/com/qa/cdstore/service/CDDBImpl.java
@@ -1,5 +1,39 @@
 package com.qa.cdstore.service;
 
+import java.util.ArrayList;
+
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.*;
+import com.qa.cdstore.model.CD;
+import com.qa.cdstore.util.JSONUtil;
+
+@Stateless
+@Default
 public class CDDBImpl implements CDService {
+
+	// ArrayList<CD>CDarray= new ArrayList();
+
+	@Inject
+	private JSONUtil JSONUtil;
+
+	@PersistenceContext(unitName = "primary")
+	private EntityManager em;
+
+	@Override
+	public String getAllCD(String cd_title) {
+		Query query = em.createQuery("SELECT cd_title FROM CD");
+		return JSONUtil.getJSONForObject(query);
+	}
+
+	@Override
+	public String addNewCD(String newCD) {
+		CD cd = JSONUtil.getObjectForJSON(newCD, CD.class);
+		em.persist(cd);
+
+		return newCD;
+
+	}
 
 }
--- src/main/java/com/qa/cdstore/service/CDServiceImpl.java
+++ src/main/java/com/qa/cdstore/service/CDServiceImpl.java
@@ -1,5 +1,31 @@
 package com.qa.cdstore.service;
 
+import java.util.Map;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Alternative;
+import javax.persistence.EntityManager;
+
+import com.qa.cdstore.model.CD;
+
+
+@ApplicationScoped
+@Alternative
+
+
 public class CDServiceImpl implements CDService {
 
+	private Map<Integer, CD>cdMap;
+
+	@Override
+	public String getAllCD(String title) {
+		
+		return null;
+	}
+
+	@Override
+	public String addNewCD(String newCD) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
