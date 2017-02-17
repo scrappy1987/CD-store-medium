77806cd371b437033347378f099a21420108e8bd
NabeelMohammed <nkushi94@hotmail.com>
2017-02-17 16:01:37 +0000

    nabeels back end

--- src/main/java/com/qa/cdstore/model/CD.java
+++ src/main/java/com/qa/cdstore/model/CD.java
@@ -1,5 +1,61 @@
 package com.qa.cdstore.model;
 
+
+import javax.persistence.Entity;
+
+import javax.persistence.Id;
+
+
+@Entity
 public class CD {
+	
+	@Id
+	private int CD_ID;
+	
+	private String CD_Title;
+
+	private String Artist;
+	
+	public CD () {}
+
+	public CD(int cD_ID, String cD_Title, String artist) {
+		super();
+		CD_ID = cD_ID;
+		CD_Title = cD_Title;
+		Artist = artist;
+	}
+
+	public int getCD_ID() {
+		return CD_ID;
+	}
+
+	public void setCD_ID(int cD_ID) {
+		CD_ID = cD_ID;
+	}
+
+	public String getCD_Title() {
+		return CD_Title;
+	}
+
+	public void setCD_Title(String cD_Title) {
+		CD_Title = cD_Title;
+	}
+
+	public String getArtist() {
+		return Artist;
+	}
+
+	public void setArtist(String artist) {
+		Artist = artist;
+	}
+
+	@Override
+	public String toString() {
+		return "CD [CD_ID=" + CD_ID + ", CD_Title=" + CD_Title + ", Artist="
+				+ Artist + ", getCD_ID()=" + getCD_ID() + ", getCD_Title()="
+				+ getCD_Title() + ", getArtist()=" + getArtist() + "]";
+	}
+
+
 
 }
--- src/main/java/com/qa/cdstore/service/CDService.java
+++ src/main/java/com/qa/cdstore/service/CDService.java
@@ -1,5 +1,10 @@
 package com.qa.cdstore.service;
 
 public interface CDService {
+	
+	String getAllCDs();
+	String addNewCD(String CDJson);
+	String replaceCD(Integer CD_ID, String updatedCD);
+	String deleteCD(Integer CD_ID);
 
 }
