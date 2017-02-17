248a94d1051ad6013335b08f70966b8db5996a99
DanSampson <DanSampson>
2017-02-17 15:55:33 +0000

    dan sampson cd store attempt

--- src/main/java/com/qa/cdstore/service/CDService.java
+++ src/main/java/com/qa/cdstore/service/CDService.java
@@ -1,5 +1,9 @@
 package com.qa.cdstore.service;
 
 public interface CDService {
-
+	String getAllCDs();
+	String addNewCD(String cdJson);
+	String replaceCD(Integer cdId, String updateCD);
+	String deleteCD(Integer cdId);
+	String getOneCD(Integer cdId);
 }
