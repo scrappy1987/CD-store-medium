b802e4d7812c9fef7eca59a3b4f2d039dd02e68f
ahmad sultan <ahmed_sultan14@hotmail.com>
2017-02-17 15:56:48 +0000

    back-end stuff

--- src/main/java/com/qa/cdstore/model/CD.java
+++ src/main/java/com/qa/cdstore/model/CD.java
@@ -1,5 +1,70 @@
 package com.qa.cdstore.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class CD {
+	
+	
+	@Id
+	@GeneratedValue
+	private long Id;
+	private String Artist;
+	private String title;
+	private String genre;
+	
+
+	public CD(String Artist, String title, String genre) {
+		
+		this.Artist = Artist;
+		this.title = title;
+		this.genre = genre;
+	}
+	
+	public CD(){
+		
+	};
+
+
+	public long getId() {
+		return Id;
+	}
+
+
+	public void setId(long id) {
+		Id = id;
+	}
+
+
+	public String getArtist() {
+		return Artist;
+	}
+
+
+	public void setArtist(String Artist) {
+		this.Artist = Artist;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getGenre() {
+		return genre;
+	}
+
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
 
 }
--- src/main/java/com/qa/cdstore/rest/CDEndpoint.java
+++ src/main/java/com/qa/cdstore/rest/CDEndpoint.java
@@ -1,5 +1,46 @@
 package com.qa.cdstore.rest;
 
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import com.qa.cdstore.service.CDService;
+
+@Path("/cdstore")
 public class CDEndpoint {
+	
+	@Inject 
+	private CDService CdService;
+	
+	@GET
+	@Path("/json")
+	@Produces({"application/json"})
+	public String getCdAsJson(){
+		return CdService.getAllCD();
+	}
+	@POST
+	@Path("/json")
+	@Produces({"application/json"})
+	public String addNewCDToMap(String CdJson){
+		return CdService.addNewCD(CdJson);
+	}
+	@PUT
+	@Path("/json/{id}")
+	@Produces({"application/json"})
+	public String replaceCDFromCDStore(@PathParam("Id") Integer Id, String CdJson) {
+		return CdService.replaceCD(Id, CdJson);
+	}
+	@DELETE 
+	@Path("/json/{id}")
+	@Produces({"application/json"})
+	public String deleteCDFromCDStore(@PathParam("id") Integer Id, String CdJson){
+		return CdService.deleteCD(Id);
+	}
+	
 
 }
